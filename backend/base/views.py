9f4364150fcb5047d57e1f2a84160b7f8b817a01
Binay Adhikari <132814477+AdBinay@users.noreply.github.com>
2023-12-08 16:41:33 +0545

    api_route_development

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view 
+from rest_framework.response import Response
 from .notice import notices
 # Create your views here.
 
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
         '/api/notices/',
@@ -16,7 +19,7 @@ def getRoutes(request):
         '/api/notices/delete/<id>/',
         '/api/notices/<update>/<id>/',
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
 
 def getnotices(request):
     return JsonResponse(notices, safe=False)
