6276ccdf1807057d9077b1a713b1acfbef4b74f4
Binay Adhikari <132814477+AdBinay@users.noreply.github.com>
2023-12-09 11:19:21 +0545

    db and data modeling

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Notice(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=200, null=False,blank=False)
+    #image
+    description = models.TextField(null=False , blank=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    _id = models.AutoField(primary_key=True, editable=False)
