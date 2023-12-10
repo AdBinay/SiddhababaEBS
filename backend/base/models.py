ceae5a34eeb90f2f7cf948979e6094032f51cb90
Binay Adhikari <132814477+AdBinay@users.noreply.github.com>
2023-12-10 08:31:00 +0545

    notice-image and library setup

--- backend/base/models.py
+++ backend/base/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Notice(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=200, null=False,blank=False)
-    #image
+    image = models.ImageField(null=True,blank=True)
     description = models.TextField(null=False , blank=False)
     createdAt = models.DateTimeField(auto_now_add=True)
     _id = models.AutoField(primary_key=True, editable=False)
