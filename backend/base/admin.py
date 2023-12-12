49176a9d2cd284f97a40c0e6fd40390347f14755
Binay Adhikari <132814477+AdBinay@users.noreply.github.com>
2023-12-12 09:41:17 +0545

    search field in admin panel

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from . models import Notice
 # Register your models here.
 
-admin.site.register(Notice)
+class NoticeAdmin(admin.ModelAdmin):
+    # Let you to search with title name, release year and length of duration of movie
+    search_fields = ['title']
+    # There will be a filter on release year
+    list_filter = ['title']
+ 
+
+admin.site.register(Notice, NoticeAdmin)
+
