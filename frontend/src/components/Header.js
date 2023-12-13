096ace7499fa806ba76a5a17b9e9337fbf56e79e
Binay Adhikari <132814477+AdBinay@users.noreply.github.com>
2023-12-13 08:22:42 +0545

    navigation-path

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -10,20 +10,26 @@ function Header() {
       <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand >Siddhababa_EBS</Navbar.Brand>
+            <Navbar.Brand >Siddhababa</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
+          <Navbar.Collapse id="basic-navbar-nav ">
+            <Nav className="mr-auto ">
               <LinkContainer to='/notices'>
                 <Nav.Link ><i className='fa fa-bell'></i> Notice</Nav.Link>
               </LinkContainer>
               <LinkContainer to='/gallery'>
                 <Nav.Link ><i className="fa-solid fa-image"></i> Gallery</Nav.Link>
               </LinkContainer>
-              {/* <LinkContainer to='/login'>
-                <Nav.Link ><i className="fa-solid fa-user"></i> Login</Nav.Link>
-              </LinkContainer> */}
+              <LinkContainer to='/downloads'>
+                <Nav.Link ><i className="fa-solid fa-download"></i> Downloads</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/curriculum'>
+                <Nav.Link ><i className="fa-solid fa-book"></i> Curriculum</Nav.Link>
+              </LinkContainer> 
+              <LinkContainer to='/faculty'>
+                <Nav.Link ><i className="fa-solid fa-users"></i> Teaching Faculty</Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
