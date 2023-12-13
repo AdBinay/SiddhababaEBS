15ab9f8da99488e51b4839f004d7c174d893a1ce
Binay Adhikari <132814477+AdBinay@users.noreply.github.com>
2023-12-13 20:11:21 +0545

    v01_notice section

--- frontend/src/screens/NewsHomeScreen.js
+++ frontend/src/screens/NewsHomeScreen.js
@@ -1,6 +1,6 @@
 import React, {useState , useEffect} from 'react';
 import { Link } from 'react-router-dom';
-import { Row, Col, Card, Image } from 'react-bootstrap';
+import { Row, Col, Card, Image, Button } from 'react-bootstrap';
 import axios from 'axios'
 // import Notice from '../components/Notice';
 
@@ -20,24 +20,24 @@ function NewsHomeScreen() {
       <Row>
         {notices.map((notice) => (
           <Col key={notice._id} sm={12} md={6} lg={4} xl={3} style={{  margin:'auto', padding:'7px' }}>
-            <Card style={{  margin:'auto', padding:'10px' }}>
-              <div style={{  margin:'auto', padding:'10px' }}>
-                <h4 style={{ textAlign: 'center' }}>{notice.title}</h4> 
-              </div> <hr/>
+            <Card style={{  margin:'auto', padding:'10px',  width: '18rem' }}>
               <div style={{ textAlign: 'center' }}>
-                <Image src={notice.image} alt={notice.title} fluid style={{ width: '200px', height: '200px' }} />
-              </div> <hr/>
-              <p style={{ textAlign: 'left' }}>
+                <Card.Img variant="top" src={notice.image} alt={notice.title}  />
+              </div>
+              <div style={{  margin:'auto', padding:'8px' }}>
+                <Card.Title>{notice.title}</Card.Title> 
+              </div> 
+              <Card.Text style={{ textAlign: 'left' }}>
                 {notice.description.length > 20 ? (
                   <span>
                     {`${notice.description.slice(0, 50)}... `}
-                    <Link to={`/notices/${notice._id}`} ><b> Read more</b></Link>
+                    <Link to={`/notices/${notice._id}`} ><br/><Button variant="info" size="sm">Read More</Button></Link>
                   </span>
                 ) : (
                   notice.description
                 )} <br/> <hr/>
-                Post on : {notice.createdAt}
-              </p>
+              </Card.Text>
+              <h6>Post on : {notice.createdAt}</h6>
             </Card>
           </Col>
         ))}
