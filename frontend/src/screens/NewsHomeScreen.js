01c754f43b37375917db8ecf38b0f7575e2c4344
Binay Adhikari <132814477+AdBinay@users.noreply.github.com>
2023-12-13 09:53:05 +0545

    ux-updated

--- frontend/src/screens/NewsHomeScreen.js
+++ frontend/src/screens/NewsHomeScreen.js
@@ -19,25 +19,24 @@ function NewsHomeScreen() {
       <h1>Latest Notice</h1> <br/>
       <Row>
         {notices.map((notice) => (
-          <Col key={notice._id} sm={12} md={6} lg={4} xl={3} style={{  margin:'auto', padding:'10px' }}>
+          <Col key={notice._id} sm={12} md={6} lg={4} xl={3} style={{  margin:'auto', padding:'7px' }}>
             <Card style={{  margin:'auto', padding:'10px' }}>
-              {/* <Notice notice={notice} /> */}
               <div style={{  margin:'auto', padding:'10px' }}>
                 <h4 style={{ textAlign: 'center' }}>{notice.title}</h4> 
               </div> <hr/>
               <div style={{ textAlign: 'center' }}>
                 <Image src={notice.image} alt={notice.title} fluid style={{ width: '200px', height: '200px' }} />
               </div> <hr/>
-              <p style={{ textAlign: 'center' }}>
+              <p style={{ textAlign: 'left' }}>
                 {notice.description.length > 20 ? (
                   <span>
                     {`${notice.description.slice(0, 50)}... `}
-                    <Link to={`/notices/${notice._id}`} ><b> Read more..</b></Link>
+                    <Link to={`/notices/${notice._id}`} ><b> Read more</b></Link>
                   </span>
                 ) : (
                   notice.description
-                )} <br/>
-                post on : {notice.date_published}
+                )} <br/> <hr/>
+                Post on : {notice.createdAt}
               </p>
             </Card>
           </Col>
