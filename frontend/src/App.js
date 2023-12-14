65460f14105cf4222d0cfdec3d3f1afb74a25474
mabisha164 <mabisha164@gmail.com>
2023-12-14 13:10:56 +0545

    Added CSS on the header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,32 @@
-import { Container } from 'react-bootstrap'
-import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer';
-import NewsHomeScreen from './screens/NewsHomeScreen'
-import NewsScreen from './screens/NewsScreen'
-import HomeScreen from './screens/HomeScreen'
-import FacultyScreen from './screens/FacultyScreen'
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import NewsHomeScreen from "./screens/NewsHomeScreen";
+import NewsScreen from "./screens/NewsScreen";
+import HomeScreen from "./screens/HomeScreen";
+import FacultyScreen from "./screens/FacultyScreen";
+import Gallery from "./components/Gallery";
 
 function App() {
   return (
-    <Router>
+    <div>
+      <Router>
         <Header />
-        <main className='py-3'>
+        <main className="py-3">
           <Container>
             <Routes>
-              <Route path='/' element={<HomeScreen />} exact/>
-              <Route path='/notices/' element={<NewsHomeScreen />} />
-              <Route path='/notices/:_id' element={<NewsScreen />} />
-              <Route path='/faculty/' element={<FacultyScreen />} />
+              <Route path="/" element={<HomeScreen />} exact />
+              <Route path="/notices/" element={<NewsHomeScreen />} />
+              <Route path="/notices/:_id" element={<NewsScreen />} />
+              <Route path="/faculty/" element={<FacultyScreen />} />
+              <Route path="/gallery/" element={<Gallery />} />
             </Routes>
           </Container>
         </main>
         <Footer />
-    </Router>
+      </Router>
+    </div>
   );
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,61 +1,71 @@
-//rfce
-
 import React from "react";
 import { LinkContainer } from "react-router-bootstrap";
 import { Navbar, Nav, Container, Row, Col } from "react-bootstrap";
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 function Header() {
   return (
     <header>
-      <Navbar bg="info" variant="dark" expand="lg" collapseOnSelect>
+      <Navbar
+        bg="warning"
+        variant="dark"
+        expand="lg"
+        collapseOnSelect
+        className="border-bottom border-light rounded shadow"
+      >
         <Container>
           <LinkContainer to="/">
             <Navbar.Brand>Siddhababa_EBS</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto ">
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="d-flex justify-content-between"
+          >
+            <Nav className="mr-auto">
               <LinkContainer to="/notices">
-                <Nav.Link>
-                  <i className="fa fa-bell"></i>
-                  Notice
+                <Nav.Link className="text-light">
+                  <i className="fa fa-bell"></i> Notice
                 </Nav.Link>
               </LinkContainer>
               <LinkContainer to="/gallery">
-                <Nav.Link>
+                <Nav.Link className="text-light">
                   <i className="fa-solid fa-image"></i> Gallery
                 </Nav.Link>
               </LinkContainer>
-              {/* <LinkContainer to='/login'>
-                <Nav.Link ><i className="fa-solid fa-user"></i> Login</Nav.Link>
-              </LinkContainer> */}
-              <LinkContainer to='/downloads'>
-                <Nav.Link ><i className="fa-solid fa-download"></i> Downloads</Nav.Link>
+              <LinkContainer to="/downloads">
+                <Nav.Link className="text-light">
+                  <i className="fa-solid fa-download"></i> Downloads
+                </Nav.Link>
               </LinkContainer>
-              <LinkContainer to='/curriculum'>
-                <Nav.Link ><i className="fa-solid fa-book"></i> Curriculum</Nav.Link>
-              </LinkContainer> 
-              <LinkContainer to='/faculty'>
-                <Nav.Link ><i className="fa-solid fa-users"></i> Teaching Faculty</Nav.Link>
-              </LinkContainer> 
-              <Form inline>
-                <Row>
-                  <Col xs="auto">
-                    <Form.Control
-                      type="text"
-                      placeholder="Search"
-                      className=" mr-sm-2"
-                    />
-                  </Col>
-                  <Col xs="auto">
-                    <Button variant="outline-danger" type="submit" >Submit</Button>
-                  </Col>
-                </Row>
-              </Form>
+              <LinkContainer to="/curriculum">
+                <Nav.Link className="text-light">
+                  <i className="fa-solid fa-book"></i> Curriculum
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/faculty">
+                <Nav.Link className="text-light">
+                  <i className="fa-solid fa-users"></i> Teaching Faculty
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
+            <Form inline className="">
+              <Row className="align-items-center">
+                <Col xs="auto" className="mb-2">
+                  <Form.Control
+                    type="text"
+                    placeholder="Search"
+                    className="mr-sm-2 rounded"
+                  />
+                </Col>
+                <Col xs="auto">
+                  <Button variant="outline-light rounded" type="submit">
+                    Submit
+                  </Button>
+                </Col>
+              </Row>
+            </Form>
           </Navbar.Collapse>
         </Container>
       </Navbar>
