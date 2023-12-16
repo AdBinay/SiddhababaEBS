568677ad37159cd1a8d00cdd254496db733ee23c
Binay Adhikari <132814477+AdBinay@users.noreply.github.com>
2023-12-16 12:40:57 +0545

    gmap-integration

--- frontend/src/components/ContactUs.js
+++ frontend/src/components/ContactUs.js
@@ -29,7 +29,7 @@ export default function FindUs() {
         <div className='col-lg-4'>
           <iframe
             title='location'
-            src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12094.57348593182!2d-74.00599512526003!3d40.72586666928451!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2598f988156a9%3A0xd54629bdf9d61d68!2sBroadway-Lafayette%20St!5e0!3m2!1spl!2spl!4v1624523797308!5m2!1spl!2spl'
+            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3527.0669311913603!2d83.81599136536101!3d27.886923808253844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2snp!4v1702709474032!5m2!1sen!2snp"
             className='h-100 w-100'
             style={{ border: '0' }}
             loading='lazy'
