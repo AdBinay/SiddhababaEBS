79dfa870c1c110ce0c7d40d8f494823580cc64a7
Binay Adhikari <132814477+AdBinay@users.noreply.github.com>
2023-12-21 13:25:12 +0545

    ux-set

--- frontend/src/components/BootstrapCarouselComponent.js
+++ frontend/src/components/BootstrapCarouselComponent.js
@@ -70,67 +70,66 @@ import { Carousel } from 'react-bootstrap';
 import axios from 'axios';
 
 const BootstrapCarouselComponent = () => {
-  const [profilepicture, setProfileimage] = useState({
-    first_image: '',
-    second_image: '',
-    third_image: ''
-  });
+  const [profilepictures, setProfilepictures] = useState([]);
 
   useEffect(() => {
-    const fetchProfileimage = async () => {
+    const fetchProfilepictures = async () => {
         const response = await axios.get('/api/profilepicture/'); // Replace with your API endpoint
-        setProfileimage(response.data);
+        setProfilepictures(response.data);
     };
-    fetchProfileimage();
+    fetchProfilepictures();
   }, []);
 
   return (
     <div>
-        
-      <div className='container-fluid'>
-        <div className="row">
-          <div className="col-12">
-            <Carousel>
-                {/* {profilepicture.map((profilepicture))} */}
-              <Carousel.Item>
-                <img
-                  className="d-block w-100"
-                  src={profilepicture.first_image}
-                  alt="First slide"
-                />
-                <Carousel.Caption>
-                  <h3>First slide label</h3>
-                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-              </Carousel.Item>
+      {profilepictures.map((profilepicture) => (
+        < div key={profilepicture.Caption}>
+           <div className='container-fluid'>
+            <div className="row">
+              <div className="col-12">
+                <Carousel>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100"
+                      src={profilepicture.first_image}
+                      alt="First slide"
+                    />
+                    <Carousel.Caption>
+                      <h3>{profilepicture.image_heading}</h3>
+                      <p>{profilepicture.image_caption}</p>
+                    </Carousel.Caption>
+                  </Carousel.Item>
 
-              <Carousel.Item>
-                <img
-                  className="d-block w-100"
-                  src={profilepicture.second_image}
-                  alt="Second slide"
-                />
-                <Carousel.Caption>
-                  <h3>Second slide label</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-              </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100"
+                      src={profilepicture.second_image}
+                      alt="Second slide"
+                    />
+                    <Carousel.Caption>
+                      <h3>{profilepicture.image_heading}</h3>
+                      <p>{profilepicture.image_caption}</p>
+                    </Carousel.Caption>
+                  </Carousel.Item>
 
-              <Carousel.Item>
-                <img
-                  className="d-block w-100"
-                  src={profilepicture.third_image}
-                  alt="Third slide"
-                />
-                <Carousel.Caption>
-                  <h3>Third slide label</h3>
-                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-            </Carousel>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100"
+                      src={profilepicture.third_image}
+                      alt="Third slide"
+                    />
+                    <Carousel.Caption>
+                      <h3>{profilepicture.image_heading}</h3>
+                      <p>{profilepicture.image_caption}</p>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                </Carousel>
+              </div>
+            </div>
           </div>
+     
         </div>
-      </div>
+      ))}
     </div>
   );
 };
