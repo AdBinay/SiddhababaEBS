83826d14f0aed873a867743a451c03da61194fca
Binay Adhikari <132814477+AdBinay@users.noreply.github.com>
2023-12-27 08:19:03 +0545

    position-adjust-header

--- frontend/src/components/TestHead.js
+++ frontend/src/components/TestHead.js
@@ -1,73 +1,80 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './OverHead.css';
-import { LinkContainer } from "react-router-bootstrap";
+import { LinkContainer } from 'react-router-bootstrap';
 import Nav from 'react-bootstrap/Nav';
-import {useState , useEffect} from 'react';
-import axios from 'axios'
+import axios from 'axios';
 
 function TestHead() {
-  const [schoolinformation, setSchoolinformation] = useState([]);
-    useEffect(() =>{
-      async function fetchSchoolinformation(){
-        const { data } = await axios.get("/api/schoolinformation/");
-        setSchoolinformation(data);
+  const [schoolInformation, setSchoolInformation] = useState([]);
+
+  useEffect(() => {
+    async function fetchSchoolInformation() {
+      try {
+        const { data } = await axios.get('/api/schoolinformation/');
+        setSchoolInformation(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
       }
-      fetchSchoolinformation();
-    },[])
-    return (
-    <div className="container-fluid TestHead ">
-      {schoolinformation.map((schoolinformation) => (
-        <div key={schoolinformation.name}>
-          <div className="row topbar pad-2 d-flex justify-content-between">
-            <div className="col-lg-7">
-              <div className=" ml-3 d-flex justify-content-around">
-              <LinkContainer to="/">
-                <Nav.Link >
-                  <img className="mr-3 rounded-circle" src={schoolinformation.school_logo} alt="SEBSS Logo" width="250" height="250" />
-                </Nav.Link>
-              </LinkContainer>
-                <div className='mt-4'>
-                <h1 className=" OpenSans-ExtraBold text-uppercase">
-                  {schoolinformation.school_name}
-                </h1 >
-                <p className=" OpenSans-ExtraBold text-uppercase">{schoolinformation.school_motto}</p>
-                <marquee className='marque'>
-                  <h2>{schoolinformation.nepali_name}</h2>
-                </marquee>
-                </div>
-              </div>
+    }
+    fetchSchoolInformation();
+  }, []);
+
+  return (
+    <>
+      {schoolInformation.map((school, index) => (
+        <div key={index} className="row topbar pad-2">
+          <div className="col-lg-2 col-md-3 col-sm-4 col-12 d-flex align-items-center">
+            <LinkContainer to="/" className="active nav-link">
+              <Nav.Link>
+                <img
+                  className="mr-3 rounded-circle logo"
+                  src="/images/logo.png"
+                  alt="SEBSS Logo"
+                  width="225"
+                  height="225"
+                />
+              </Nav.Link>
+            </LinkContainer>
+          </div>
+          <div className="col-lg-8 col-md-6 col-sm-8 col-12 d-flex align-items-center">
+            <div className="ml-3">
+              <h1 className="OpenSans-ExtraBold text-uppercase">
+                {school.school_name}
+              </h1>
+              <p className="OpenSans-ExtraBold text-uppercase">
+                {school.school_motto}
+              </p>
+              <marquee className="marquee">
+                <h2>{school.nepali_name}</h2>
+              </marquee>
             </div>
-                <div className='col-lg-3 mt-4'>
-                    <ul className='list-unstyled '>
-                      <li>
-                        <i className='fas fa-map-marker-alt fa-2x text-primary'></i>
-                        <p>
-                          <small className='text-dark'>{schoolinformation.school_address}</small>
-                        </p>
-                      </li>
-                      <li>
-                        <i className='fas fa-phone fa-2x text-primary'></i>
-                        <p>
-                          <small className='text-dark'>{schoolinformation.school_phone}</small>
-                        </p>
-                      </li>
-                      <li>
-                        <i className='fas fa-envelope fa-2x text-primary'></i>
-                        <p>
-                          <small className='text-dark'>{schoolinformation.school_email}</small>
-                        </p>
-                      </li>
-                    </ul>
-                </div>
-          </div> 
+          </div>
+          <div className="col-lg-2 col-md-3 col-sm-12 col-12 d-flex flex-column align-items-center">
+            <ul className="list-unstyled contact-info">
+              <li>
+                <i className="fas fa-phone fa-lg text-primary"></i>
+                <p>
+                  <small className="text-dark">{school.school_phone}</small>
+                </p>
+              </li>
+              <li>
+                <i className="fas fa-envelope fa-lg text-primary"></i>
+                <p>
+                  <small className="text-dark">{school.school_email}</small>
+                </p>
+              </li>
+              <li style={{ marginTop: '10px' }}>
+                <i className="fas fa-map-marker-alt fa-lg text-primary"></i>
+                <p>
+                  <small className="text-dark">{school.school_address}</small>
+                </p>
+              </li>
+            </ul>
+          </div>
         </div>
-        ))}
-    </div>
-    
+      ))}
+    </>
   );
 }
 
 export default TestHead;
-
-
-
