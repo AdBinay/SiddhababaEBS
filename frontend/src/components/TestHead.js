1b850420af2039b5ca13190e7e3799ea73df4e3d
Binay Adhikari <132814477+AdBinay@users.noreply.github.com>
2023-12-30 08:56:55 +0545

    Header_updated

--- frontend/src/components/TestHead.js
+++ frontend/src/components/TestHead.js
@@ -1,3 +1,92 @@
+// import React, { useState, useEffect } from 'react';
+// import './OverHead.css';
+// import { LinkContainer } from 'react-router-bootstrap';
+// import Nav from 'react-bootstrap/Nav';
+// import axios from 'axios';
+
+// function TestHead() {
+//   const [schoolInformation, setSchoolInformation] = useState([]);
+
+//   useEffect(() => {
+//     async function fetchSchoolInformation() {
+//       try {
+//         const { data } = await axios.get('/api/schoolinformation/');
+//         setSchoolInformation(data);
+//       } catch (error) {
+//         console.error('Error fetching data:', error);
+//       }
+//     }
+//     fetchSchoolInformation();
+//   }, []);
+
+//   return (
+//     <>
+//       {schoolInformation.map((school, index) => (
+//         <div key={index} className="row topbar pad-2" style={{ marginTop: '10px' }}>
+//           <div className="col-lg-2 col-md-3 col-sm-4 col-12 d-flex align-items-center">
+//             <LinkContainer to="/" className="active nav-link">
+//               <Nav.Link>
+//                 <img
+//                   className="mr-3 rounded-circle logo"
+//                   src="/images/logo.png"
+//                   alt="SEBSS Logo"
+//                   width="225"
+//                   height="225"
+//                   style={{ margin: '5px' }}
+//                 />
+//               </Nav.Link>
+//             </LinkContainer>
+//           </div>
+//           <div className="col-lg-8 col-md-6 col-sm-8 col-12 d-flex align-items-center">
+//             <div className="ml-3">
+//               <h1 className="OpenSans-ExtraBold text-uppercase" style={{ fontSize: '42px' }}>
+//                 {school.school_name}
+//               </h1>
+//               <p style={{ fontSize: '24px' }}>
+//                 <i>{school.school_motto}</i>
+//               </p>
+//               {/* <marquee className="marquee">
+//                 <h2>{school.nepali_name}</h2>
+//               </marquee> */}
+//             </div>
+//           </div>
+//           <div className="col-lg-2 col-md-3 col-sm-12 col-12 d-flex flex-column align-items-center" >
+//             <ul className="list-unstyled contact-info" style={{ justifyContent: 'flex-end' }}>
+//               <li>
+//                 <i className="fa-solid fa-phone"></i>
+//                 <p>
+//                   <small className="text-dark">{school.school_phone}</small>
+//                 </p>
+//               </li>
+//               <li>
+//                 <i className="fa-solid fa-envelope"></i>
+//                 <p>
+//                   <small className="text-dark">{school.school_email}</small>
+//                 </p>
+//               </li>
+//               <li >
+//                 <i className="fa-solid fa-location-dot"></i>
+//                 <p>
+//                   <small className="text-dark">{school.school_address}</small>
+//                 </p>
+//               </li>
+//               <li >
+//                 <i className="fa-brands fa-facebook"></i>
+//                 <p>
+//                   <small className="text-dark">facebook.com/SiddhababaSchool</small>
+//                 </p>
+//               </li>
+//             </ul>
+//           </div>
+//         </div>
+//       ))}
+//     </>
+//   );
+// }
+
+// export default TestHead;
+
+
 import React, { useState, useEffect } from 'react';
 import './OverHead.css';
 import { LinkContainer } from 'react-router-bootstrap';
@@ -6,6 +95,7 @@ import axios from 'axios';
 
 function TestHead() {
   const [schoolInformation, setSchoolInformation] = useState([]);
+  const [currentDateTime, setCurrentDateTime] = useState(new Date());
 
   useEffect(() => {
     async function fetchSchoolInformation() {
@@ -16,8 +106,18 @@ function TestHead() {
         console.error('Error fetching data:', error);
       }
     }
+
+    const interval = setInterval(() => {
+      setCurrentDateTime(new Date());
+    }, 1000);
+
     fetchSchoolInformation();
+
+    return () => clearInterval(interval);
   }, []);
+
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  const dateTimeStyle = { fontSize: '20px' }; // Adjust the font size here
 
   return (
     <>
@@ -43,31 +143,40 @@ function TestHead() {
                 {school.school_name}
               </h1>
               <p style={{ fontSize: '24px' }}>
-                <i>{school.school_motto}</i>
+                <i>{school.nepali_name}</i>
               </p>
-              <marquee className="marquee">
-                <h2>{school.nepali_name}</h2>
-              </marquee>
+              {/* Date and Time */}
+              <p>
+                <small className="text-dark" style={dateTimeStyle}>
+                  {currentDateTime.toLocaleDateString(undefined, options)} | {currentDateTime.toLocaleTimeString()}
+                </small>
+              </p>
             </div>
           </div>
           <div className="col-lg-2 col-md-3 col-sm-12 col-12 d-flex flex-column align-items-center">
-            <ul className="list-unstyled contact-info">
+            <ul className="list-unstyled contact-info" style={{ justifyContent: 'flex-end' }}>
               <li>
-                <i className="fas fa-phone fa-lg text-primary"></i>
+                <i className="fa-solid fa-phone"></i>
                 <p>
                   <small className="text-dark">{school.school_phone}</small>
                 </p>
               </li>
               <li>
-                <i className="fas fa-envelope fa-lg text-primary"></i>
+                <i className="fa-solid fa-envelope"></i>
                 <p>
                   <small className="text-dark">{school.school_email}</small>
                 </p>
               </li>
-              <li >
-                <i className="fas fa-map-marker-alt fa-lg text-primary"></i>
+              <li>
+                <i className="fa-solid fa-location-dot"></i>
                 <p>
                   <small className="text-dark">{school.school_address}</small>
+                </p>
+              </li>
+              <li>
+                <i className="fa-brands fa-facebook"></i>
+                <p>
+                  <small className="text-dark">facebook.com/SiddhababaSchool</small>
                 </p>
               </li>
             </ul>
@@ -79,3 +188,4 @@ function TestHead() {
 }
 
 export default TestHead;
+
